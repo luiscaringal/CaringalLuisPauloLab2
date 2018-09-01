49bff8397a55602aa0e6e2c882f1af30c2e8221e
luiscaringal <2015081629@ust-ics.mygbiz.com>
2018-09-01 14:39:13 +0800

    Laboratory Activity Reupload

--- app/src/main/java/com/caringal/luispaulo/MainActivity.java
+++ app/src/main/java/com/caringal/luispaulo/MainActivity.java
@@ -3,11 +3,15 @@ package com.caringal.luispaulo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.caringal.luispaulo.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.relative_layout);
+        setContentView(R.layout.constraint_layout);
+//        setContentView(R.layout.linear_layout);
     }
 }
