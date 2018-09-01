bb26d94121738a199dc425d0962ae07eefb2c808
luiscaringal <2015081629@ust-ics.mygbiz.com>
2018-09-01 14:50:54 +0800

    Reuploaded Old Files

--- app/src/main/java/com/caringal/luispaulo/MainActivity.java
+++ app/src/main/java/com/caringal/luispaulo/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.relative_layout);
-        setContentView(R.layout.constraint_layout);
+        setContentView(R.layout.relative_layout);
+//        setContentView(R.layout.constraint_layout);
 //        setContentView(R.layout.linear_layout);
     }
 }
